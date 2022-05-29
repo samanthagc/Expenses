b14540fae09b85fbd95b63be24f1514da4742d0e
Samantha <samgomescosta@gmail.com>
2022-05-29 02:55:22 -0300

    integrates category list adapter

--- app/src/main/java/br/com/sam/expenses/feature/detailedentries/ui/DetailedEntriesActivity.kt
+++ app/src/main/java/br/com/sam/expenses/feature/detailedentries/ui/DetailedEntriesActivity.kt
@@ -3,10 +3,13 @@ package br.com.sam.expenses.feature.detailedentries.ui
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import br.com.sam.expenses.R
 import br.com.sam.expenses.commons.ExpensesApi
 import br.com.sam.expenses.feature.detailedentries.model.Category
 import br.com.sam.expenses.feature.entries.model.Entry
+import br.com.sam.expenses.feature.entries.ui.EntriesAdapter
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -14,8 +17,11 @@ import retrofit2.Response
 
 class DetailedEntriesActivity : AppCompatActivity() {
 
-    private lateinit var entriesData: ArrayList<Entry>
-    private lateinit var eentriesData: List<Entry>
+    private lateinit var entriesData: List<Entry>
+
+    private val categoriesList: RecyclerView by lazy {
+        findViewById(R.id.rv_categories)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +32,7 @@ class DetailedEntriesActivity : AppCompatActivity() {
 
     private fun getBundle() {
         val args = intent.getBundleExtra(DETAILS_BUNDLE)
-        eentriesData = args!!.getSerializable(ENTRIES_EXTRA) as List<Entry>? ?: listOf()
+        entriesData = args!!.getSerializable(ENTRIES_EXTRA) as List<Entry>? ?: listOf()
     }
 
     private fun callApi() {
@@ -43,10 +49,15 @@ class DetailedEntriesActivity : AppCompatActivity() {
                 response: Response<List<Category>?>
             ) {
                 response.body()?.let {
-                    Log.e("Category API 200", it[0].name ?: "null")
+                    setupAdapter(it)
                 }
             }
         })
+    }
+
+    private fun setupAdapter(data: List<Category>) {
+        categoriesList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        categoriesList.adapter = CategoryAdapter(data)
     }
 
     companion object {
